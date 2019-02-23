c83c5611e537b65033661e1af1a45c3dbb9a9066
Akanksha <akankshasingh7095@gmail.com>
2019-02-23 20:01:22 +0530

    This is a header file.

---
+++ defs.h
@@ -0,0 +1,20 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+typedef unsigned long long U64;
+
+#define NAME "Vice 1.0"
+#define BRD_SQ_NUM 120
+
+enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK};
+enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
+enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
+
+enum { WHITE, BLACK, BOTH };
+
+
+
+
+
+
+#endif
